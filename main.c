e48068e171f2991b91a3be471b49618129d2ac24
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-06-13 13:12:07 +0100

    Update main.c

--- main.c
+++ main.c
@@ -35,24 +35,24 @@ char *cshell_read_line(void)
         char *buffer = malloc(sizeof(char) * bufsize);
         int ;
 
-        if (!buffer) 
+        if (!buffer)
         {
                 fprintf(stderr, "cshell: allocation error\n");
                 exit(EXIT_FAILURE);
         }
 
-        while (1) 
+        while (1)
         {
                 /* Read in a character in the infinite loop */
                 cha = getchar();
 
                 // If we hit EOF, replace it with a null character and return.
-                if (cha == EOF || cha == '\n') 
+                if (cha == EOF || cha == '\n')
                 {
                         buffer[position] = '\0';
                         return buffer;
                 }
-                else 
+                else
                 {
                         buffer[position] = cha;
                 }
@@ -78,7 +78,7 @@ char **cshell_split_line(char *line)
         char **tokens = malloc(bufsize * sizeof(char*));
         char *token;
 
-        if (!tokens) 
+        if (!tokens)
         {
                 fprintf(stderr, "lsh: allocation error\n");
                 exit(EXIT_FAILURE);
@@ -114,7 +114,7 @@ int cshell_launch(char **args)
         int status;
 
         pid = fork();
-        if (pid == 0) 
+        if (pid == 0)
         {
                 /* Child process */
                 if (execvp(args[0], args) == -1)
@@ -123,7 +123,7 @@ int cshell_launch(char **args)
                 }
                 exit(EXIT_FAILURE);
         }
-        else if (pid < 0) 
+        else if (pid < 0)
         {
                 /* Error forking */
                 perror("cshell");
@@ -138,3 +138,85 @@ int cshell_launch(char **args)
 
         return 1;
 }
+
+
+/*
+  List of builtin commands, followed by their corresponding functions.
+ */
+char *builtin_string[] = {
+        "cd",
+        "help",
+        "exit"
+};
+
+int (*builtin_funcs[]) (char **) = {
+        &shell_cd,
+        &shell_help,
+        &shell_exit
+};
+
+int cshell_builtin_num()
+{
+        return sizeof(builtin_string) / sizeof(char *);
+}
+
+/*
+  Builtin function implementations as follows:.
+*/
+int cshell_cd(char **args)
+{
+        if (args[1] == NULL)
+        {
+                fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+        } 
+        else 
+        {
+                if (chdir(args[1]) != 0)
+                {
+                        perror("cshell");
+                }
+        }
+        return (1);
+}
+
+int cshell_help(char **args)
+{
+        int i;
+        printf("A Simple Shell Written in C\n");
+        printf("Type program names and desired arguments, then press enter.\n");
+        printf("The following are the built in programs:\n");
+
+        for (i = 0; i < cshell_builtin_num(); i++)
+        {
+                printf("  %s\n", builtin_string[i]);
+        }
+
+        return (1);
+}
+
+int cshell_exit(char **args)
+{
+        return (0);
+}
+
+int cshell_execute(char **args)
+{
+        int i;
+
+        if (args[0] == NULL)
+        {
+                /* An empty command was entered */
+                return (1);
+        }
+
+        for (i = 0; i < lsh_num_builtins(); i++) 
+        {
+                if (strcmp(args[0], builtin_string[i]) == 0)
+                {
+                        return ((*builtin_funcs[i])(args));
+                }
+        }
+
+        return (cshell_launch(args));
+}
+
