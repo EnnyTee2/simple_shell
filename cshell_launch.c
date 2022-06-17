0669e7c3d46b6cff85faebfcb3483b4e4c7ba33b
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-06-17 21:38:11 +0100

    Update cshell_launch.c

--- cshell_launch.c
+++ cshell_launch.c
@@ -8,31 +8,31 @@
 
 int cshell_launch(char **args)
 {
-        pid_t pid;
-        int status;
+	pid_t pid;
+	int status;
 
-        pid = fork();
-        if (pid == 0)
-        {
-                /* Child process */
-                if (execvp(args[0], args) == -1)
-                {
-                        perror("cshell:");
-                }
-                exit(EXIT_FAILURE);
-        }
-        else if (pid < 0)
-        {
-                /* Error forking */
-                perror("cshell:");
-        }
-        else
-        {
-                 /* Parent process */ 
-                do {
-                        waitpid(pid, &status, WUNTRACED);
-                } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-        }
+	pid = fork();
+	if (pid == 0)
+	{
+		/* Child process */
+		if (execvp(args[0], args) == -1)
+		{
+			perror("cshell:");
+		}
+		exit(EXIT_FAILURE);
+	}
+	else if (pid < 0)
+	{
+		/* Error forking */
+		perror("cshell:");
+	}
+	else
+	{
+		/* Parent process */
+		do {
+			waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
 
-        return(1);
+	return(1);
 }
