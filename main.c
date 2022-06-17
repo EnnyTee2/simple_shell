4a33265071ff79e747672724c1359c3d7e07bfc6
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-06-17 03:15:14 +0100

    Update main.c

--- main.c
+++ main.c
@@ -5,6 +5,28 @@
  * Return: exit_success (0)
  */
 
+/*
+  List of builtin commands, followed by their corresponding functions.
+ */
+
+char *builtin_string[] = {
+        "cd",
+        "help",
+        "exit"
+};
+
+int (*builtin_funcs[]) (char **) = {
+        &cshell_cd,
+        &cshell_help,
+        &cshell_exit
+};
+
+int cshell_builtin_num()
+{
+        return (sizeof(builtin_string) / sizeof(char *));
+}
+
+
 int main()
 {
         /* start shell loop. */
