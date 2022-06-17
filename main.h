3bc863475a8f3db4c517888605d60d23e86982d8
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-06-17 01:25:53 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-#ifndef _MAIN_H_
-#define _MAIN_H_
-#define SHELL_BUFSIZE 1024
-#define TOKEN_BUFSIZE 64
-#define TOKEN_DELIMITER " \t\r\n\a"
-
-#include <stdarg.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <string.h>
-#include <sys/wait.h>
-
-void cshell_loop(void);
-char *cshell_read_line(void);
-char **cshell_split_line(char *line);
-
-int cshell_launch(char **args);
-
-int cshell_builtins();
-int cshell_cd(char **args);
-int cshell_help(char **args);
-int cshell_exit(char **args);
-int cshell_execute(char **args);
-int cshell_builtin_num();
-
-#endif
-=======
 #ifndef _MAIN_H_
 #define _MAIN_H_
 #define SHELL_BUFSIZE 1024
@@ -77,4 +47,3 @@ int cshell_builtin_num()
 }
 
 #endif
->>>>>>> 3a74c2e1adfdc24530fed4fabc5fb7475e7be479
