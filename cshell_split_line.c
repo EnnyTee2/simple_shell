665b1c57f44205b4465befe1d5a4d04e02779458
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-06-17 22:14:49 +0100

    Update cshell_split_line.c

--- cshell_split_line.c
+++ cshell_split_line.c
@@ -9,37 +9,36 @@
  */
 
 char **cshell_split_line(char *line)
-{
-        int bufsize = TOKEN_BUFSIZE, position = 0;
-        char **tokens = malloc(bufsize * sizeof(char*));
-        char *token;
+	{
+	int bufsize = TOKEN_BUFSIZE, position = 0;
+	char **tokens = malloc(bufsize * sizeof(char *));
+	char *token;
 
-        if (!tokens)
-        {
-                fprintf(stderr, "cshell: allocation error\n");
-                exit(EXIT_FAILURE);
-        }
+	if (!tokens)
+	{
+		fprintf(stderr, "cshell: allocation error\n");
+		exit(EXIT_FAILURE);
+	}
 
-        token = strtok(line, TOKEN_DELIMITER);
+	token = strtok(line, TOKEN_DELIMITER);
 
-        while (token != NULL)
-        {
-                tokens[position] = token;
-                position++;
+	while (token != NULL)
+	{
+		tokens[position] = token;
+		position++;
 
-                if (position >= bufsize)
-                {
-                        bufsize += TOKEN_BUFSIZE;
-                        tokens = realloc(tokens, bufsize * sizeof(char*));
-                        if (!tokens)
-                        {
-                                fprintf(stderr, "cshell: allocation error\n");
-                                exit(EXIT_FAILURE);
-                        }
-                }
- 
-                token = strtok(NULL, TOKEN_DELIMITER);
-        }
-        tokens[position] = NULL;
-        return (tokens);
+		if (position >= bufsize)
+		{
+			bufsize += TOKEN_BUFSIZE;
+			tokens = realloc(tokens, bufsize * sizeof(char *));
+			if (!tokens)
+			{
+				fprintf(stderr, "cshell: allocation error\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+		token = strtok(NULL, TOKEN_DELIMITER);
+	}
+	tokens[position] = NULL;
+	return (tokens);
 }
