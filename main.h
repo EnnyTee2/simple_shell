7bb909d1114628c877a57c0e018341588168a724
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-06-17 20:16:50 +0100

    Create main.h

---
+++ main.h
@@ -0,0 +1,27 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#define SHELL_BUFSIZE 1024
+#define TOKEN_BUFSIZE 64
+#define TOKEN_DELIMITER " \t\r\n\a"
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/wait.h>
+
+void cshell_loop(void);
+char *cshell_read_line(void);
+char **cshell_split_line(char *line);
+
+int cshell_launch(char **args);
+int cshell_builtins();
+int cshell_cd(char **args);
+int cshell_help();
+int cshell_exit();
+int cshell_execute(char **args);
+int cshell_builtin_num();
+
+#endif
