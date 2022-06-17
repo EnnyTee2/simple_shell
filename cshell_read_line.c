13909e7ab901415064f62a087dfac19a2e0cd117
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-06-17 21:51:21 +0100

    Update cshell_read_line.c

--- cshell_read_line.c
+++ cshell_read_line.c
@@ -2,52 +2,49 @@
 
 /**
  * cshell_read_line - function to read line
- * @bufsize: buffer size
- * @position: char position in buffer
- * @buffer: memory allocation for buffer
- * @cha: function call to read character
+ * Return: buffer (allocated memory)
  */
 
 char *cshell_read_line(void)
 {
-        int bufsize = SHELL_BUFSIZE;
-        int position = 0;
-        char *buffer = malloc(sizeof(char) * bufsize);
-        int cha;
+	int bufsize = SHELL_BUFSIZE;
+	int position = 0;
+	char *buffer = malloc(sizeof(char) * bufsize);
+	int cha;
 
-        if (!buffer)
-        {
-                fprintf(stderr, "cshell: allocation error\n");
-                exit(EXIT_FAILURE);
-        }
+	if (!buffer)
+	{
+		fprintf(stderr, "cshell: allocation error\n");
+		exit(EXIT_FAILURE);
+	}
 
-        while (1)
-        {
-                /* Read in a character in the infinite loop */
-                cha = getchar();
+	while (1)
+	{
+		/* Read in a character in the infinite loop */
+		cha = getchar();
 
-                /* If it gets EOF, replace it with a null character and return. */
-                if (cha == EOF || cha == '\n')
-                {
-                        buffer[position] = '\0';
-                        return buffer;
-                }
-                else
-                {
-                        buffer[position] = cha;
-                }
-                position++;
+		/* If it gets EOF, replace it with null character, return */
+		if (cha == EOF || cha == '\n')
+		{
+			buffer[position] = '\0';
+			return (buffer);
+		}
+		else
+		{
+			buffer[position] = cha;
+		}
+		position++;
 
-                /* If buffer size exceeded, reallocate buffer. */
-                if (position >= bufsize)
-                {
-                        bufsize += SHELL_BUFSIZE;
-                        buffer = realloc(buffer, bufsize);
-                        if (!buffer)
-                        {
-                                fprintf(stderr, "cshell: allocation error\n");
-                                exit(EXIT_FAILURE);
-                        }
-                }
-        }
+		/* If buffer size exceeded, reallocate buffer. */
+		if (position >= bufsize)
+		{
+			bufsize += SHELL_BUFSIZE;
+			buffer = realloc(buffer, bufsize);
+			if (!buffer)
+			{
+				fprintf(stderr, "cshell: allocation error\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
 }
