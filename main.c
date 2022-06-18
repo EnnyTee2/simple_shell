52ecebd8395bc109cd19c50508b58f6f4ee1256c
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-06-18 02:21:41 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,73 @@
 #include "main.h"
 
 /**
- * main - entry point
- * Return: exit_success (0)
+ * free_data - frees data structure
+ *
+ * @datash: data structure
+ * Return: no return
  */
-
-int main(void)
+void free_data(data_shell *datash)
 {
-	/* start shell loop. */
-	cshell_loop();
+	unsigned int i;
 
-	/* shutdown/cleanup. */
-	return (EXIT_SUCCESS);
+	for (i = 0; datash->_environ[i]; i++)
+	{
+		free(datash->_environ[i]);
 	}
+
+	free(datash->_environ);
+	free(datash->pid);
+}
+
+/**
+ * set_data - Initialize data structure
+ *
+ * @datash: data structure
+ * @av: argument vector
+ * Return: no return
+ */
+void set_data(data_shell *datash, char **argv)
+{
+	unsigned int i;
+
+	datash->argv = argv;
+	datash->input = NULL;
+	datash->args = NULL;
+	datash->status = 0;
+	datash->counter = 1;
+
+	for (i = 0; environ[i]; i++)
+		;
+
+	datash->_environ = malloc(sizeof(char *) * (i + 1));
+
+	for (i = 0; environ[i]; i++)
+	{
+		datash->_environ[i] = _strdup(environ[i]);
+	}
+
+	datash->_environ[i] = NULL;
+	datash->pid = aux_itoa(getpid());
+}
+
+/**
+ * main - Entry point
+ *
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success.
+ */
+int main(int argc, char **argv)
+{
+	data_shell datash;
+	(void) argc;
+
+	signal(SIGINT, get_sigint);
+	set_data(&datash, argv);
+	shell_loop(&datash);
+	free_data(&datash);
+	if (datash.status < 0)
+		return (255);
+	return (datash.status);
+}
